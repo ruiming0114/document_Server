1c9129f245948c3eba9562e30daf23a499caf84c
Abyss948 <2292352873@qq.com>
2020-08-10 19:17:27 +0800

    添加Comment类

--- src/main/java/com/document/pojo/Comment.java
+++ src/main/java/com/document/pojo/Comment.java
@@ -1,5 +1,20 @@
 package com.document.pojo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Comment {
-    //先测试一下，随便写写
+    private int commentid;
+    private int docid;
+    private int userid;
+    private String content;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Timestamp commenttime;
 }
